dffda2ad270285dfd0a03c9a66aa6c5586d73c26
Vaishnavi <vaishnavisingh0223@gmail.com>
2025-08-19 17:56:11 +0530

    green to blue

--- src/components/html/navbar.jsx
+++ src/components/html/navbar.jsx
@@ -27,7 +27,7 @@ function Navbar({ onBrochureClick }) {
         <div className="nav-item"><i className="ri-home-9-line"></i> Home</div>
         <a href="#price"><div className="nav-item"><i className="ri-money-rupee-circle-line"></i> Price</div></a>
         <a href="#site-plan"><div className="nav-item"><i className="ri-building-line"></i> Site Plan</div></a>
-        <a href="#amenities"><div className="nav-item"><i className="ri-wifi-line"></i> Amenities</div></a>
+
         <a href="#location"><div className="nav-item"><i className="ri-map-pin-2-line"></i> Location</div></a>
         <div className="nav-item" onClick={onBrochureClick}><i className="ri-download-2-line"></i> Brochure</div>
       </div>
